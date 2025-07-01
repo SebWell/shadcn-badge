5c92869694cac878deae2feb51589660bff1c551
SebWell <sebastien.mignot@gmail.com>
2025-07-01 02:53:26 +0200

    FIX: Add complete Weweb configuration with all component options

--- ww-config.js
+++ ww-config.js
@@ -1,18 +1,78 @@
 export default {
     editor: {
-        label: 'badge',
+        label: 'Shadcn Badge',
         icon: 'tag',
     },
     triggerEvents: [
         { name: 'click', label: 'On Click', event: { value: '' }, default: true },
+        { name: 'dismiss', label: 'On Dismiss', event: { value: '' } },
     ],
     properties: {
         content: {
-            label: 'Content',
+            label: 'Badge Settings',
             type: 'Object',
             section: 'settings',
             bindable: true,
-            defaultValue: {}
+            defaultValue: {
+                text: 'Badge',
+                variant: 'default',
+                size: 'default',
+                icon: null,
+                iconPosition: 'left',
+                dismissible: false,
+                customClasses: ''
+            },
+            options: {
+                item: {
+                    text: {
+                        label: 'Text',
+                        type: 'Text',
+                        section: 'settings',
+                        bindable: true,
+                        defaultValue: 'Badge'
+                    },
+                    variant: {
+                        label: 'Variant',
+                        type: 'TextSelect',
+                        section: 'settings',
+                        options: {
+                            choices: [
+                                { value: 'default', label: 'Default' },
+                                { value: 'secondary', label: 'Secondary' },
+                                { value: 'destructive', label: 'Destructive' },
+                                { value: 'outline', label: 'Outline' }
+                            ]
+                        },
+                        defaultValue: 'default'
+                    },
+                    size: {
+                        label: 'Size',
+                        type: 'TextSelect',
+                        section: 'settings',
+                        options: {
+                            choices: [
+                                { value: 'default', label: 'Default' },
+                                { value: 'sm', label: 'Small' },
+                                { value: 'lg', label: 'Large' }
+                            ]
+                        },
+                        defaultValue: 'default'
+                    },
+                    dismissible: {
+                        label: 'Dismissible',
+                        type: 'OnOff',
+                        section: 'settings',
+                        defaultValue: false
+                    },
+                    customClasses: {
+                        label: 'Custom Classes',
+                        type: 'Text',
+                        section: 'style',
+                        bindable: true,
+                        defaultValue: ''
+                    }
+                }
+            }
         }
     }
 };
